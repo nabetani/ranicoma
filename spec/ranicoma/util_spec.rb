8c9e8a13686aa4656000a7118267fd5f4dbdb0dd
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-07-06 16:09:37 +0900

    スペック追加

--- spec/ranicoma/util_spec.rb
+++ spec/ranicoma/util_spec.rb
@@ -3,15 +3,40 @@
 require 'spec_helper'
 
 RSpec.describe Ranicoma::Util do
-  class T
-    include Ranicoma::Util
-  end
+  include Ranicoma::Util
 
   describe "#element" do
-    it "creates REXML::Element Object" do
-      hoge=T.new.element("hoge")
+    it "creates Element without attributes" do
+      hoge=element("hoge")
+      expect( REXML::Element ).to be === hoge
       expect( hoge.name ).to eq("hoge")
       expect( hoge.elements ).to be_empty
+    end
+
+    it "creates Element with attributes" do
+      hoge=element("hoge", foo:"bar", baz:"qux")
+      expect( REXML::Element ).to be === hoge
+      expect( hoge.name ).to eq("hoge")
+      expect( hoge.attributes.size ).to eq(2)
+      expect( hoge.attributes["foo"] ).to eq("bar")
+      expect( hoge.attributes["baz"] ).to eq("qux")
+    end
+
+    it "creates Element with a child" do
+      hoge=element("hoge"){ element("fuga") }
+      expect( REXML::Element ).to be === hoge
+      expect( hoge.name ).to eq("hoge")
+      expect( hoge.children.size ).to eq(1)
+      expect( hoge.children[0].name ).to eq("fuga")
+    end
+
+    it "creates Element with chlidren" do
+      hoge=element("hoge"){[ element("fuga"), element("piyo") ]}
+      expect( REXML::Element ).to be === hoge
+      expect( hoge.name ).to eq("hoge")
+      expect( hoge.children.size ).to eq(2)
+      expect( hoge.children[0].name ).to eq("fuga")
+      expect( hoge.children[1].name ).to eq("piyo")
     end
   end
 end
