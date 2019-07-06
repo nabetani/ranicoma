9bce8c3bb60fb7c2dee72a0567c221e6de5f1b2b
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-07-07 08:19:50 +0900

    線を細くする

--- lib/ranicoma/design/polypoly.rb
+++ lib/ranicoma/design/polypoly.rb
@@ -8,7 +8,15 @@ require "ranicoma/rect"
 module Ranicoma
   module Design
     class PolyPoly < Base
-      LINE = 1.0/30
+
+      def initialize(rng)
+        super
+        @psize = rng.rand(6..20)
+        @line_width = 1.0/(@psize*5)
+      end
+
+      attr_reader(:psize)
+      attr_reader(:line_width)
 
       def colors(n)
         f = lambda{ |t0|
@@ -28,7 +36,6 @@ module Ranicoma
       end
 
       def poly(center, r)
-        psize = rng.rand(6..20)
         delta = Math::PI*2/psize*0.7
         dirs = Array.new(psize){ |ix| Math::PI*2*ix / psize + rng.rand(delta) }
         outer = dirs.map{ |e|
@@ -37,7 +44,7 @@ module Ranicoma
         edelta = rng.rand(-0.2..0.2) * Math::PI*2
         ri = r * rng.rand(0.2..0.5)
         cd = rng.rand(Math::PI)
-        rdelta = rng.rand((r-ri-LINE)*0.8)
+        rdelta = rng.rand((r-ri-line_width)*0.8)
         inner_cx = center[0] + Math.cos(cd)*rdelta
         inner_cy = center[1] + Math.sin(cd)*rdelta
         inner = dirs.map{ |e|
@@ -49,7 +56,7 @@ module Ranicoma
         center_poly = element( "polygon",
           points:points_str(inner),
           **fill(%i(black white gray).sample( random:rng)),
-          **stroke(:black, LINE),
+          **stroke(:black, line_width),
           "stroke-linejoin": :round )
         Array.new( outer.size ){ |ix|
           if ! use[ix]
@@ -65,7 +72,7 @@ module Ranicoma
               "polygon",
               points:points_str(pts),
               **fill(cols.pop),
-              **stroke(:black, LINE),
+              **stroke(:black, line_width),
               "stroke-linejoin": :round )
           end
         } + [center_poly]
@@ -73,7 +80,7 @@ module Ranicoma
 
       def create
         [
-          poly([0.5,0.5],0.5-LINE)
+          poly([0.5,0.5],0.5-line_width)
         ].flatten
       end
     end
