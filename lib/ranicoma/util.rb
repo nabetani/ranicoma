15e9f71743e3ae4f3acd5df78b374b4a3ef0f25b
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-07-06 16:18:38 +0900

    コメント書く

--- lib/ranicoma/util.rb
+++ lib/ranicoma/util.rb
@@ -2,6 +2,12 @@ require 'rexml/document'
 
 module Ranicoma
   module Util
+    # REXML::Element を作る
+    # @param [Object] name タグ名。to_s で文字列にされる。
+    # @param [Hash] opt アトリビュート。key も value も to_s で文字列にされる
+    # @yield [] 子要素作るブロック
+    # @yieldreturn [REXML::Element, Array<REXML::Element>] 子要素または子要素の配列
+    # @return [REXML::Element] XML Element。
     def element(name, opt={})
       r=opt.each.with_object(REXML::Element.new(name)) do |(k,v),e|
         e.add_attribute(k.to_s, v.to_s)
